84f1d6c34d9ff7d8bc35dc126db41fa0da14db93
petarchonkov <pchonkov@gmail.com>
2019-01-28 14:45:32 +0200

    Fixed bugs and added comments to tags.py from challange 03

--- 03/tags.py
+++ 03/tags.py
@@ -10,17 +10,25 @@ SIMILAR = 0.87
 TAG_HTML = re.compile(r'<category>([^<]+)</category>')
 
 
+
 def get_tags():
     """Find all tags (TAG_HTML) in RSS_FEED.
     Replace dash with whitespace.
     Hint: use TAG_HTML.findall"""
     try:
+        # Open the file and read the entire content
         with open(RSS_FEED,'r') as f:
             s=f.read()
+            # Find all occurences of the regular expression TAG_HTML
             tags=TAG_HTML.findall(s)
+            # Find all words in the list with special chars and replace them with space
+            # Change all words in the list to lowercase
+            for index,tag in enumerate(tags):
+                tags[index]=re.sub(r'[^a-zA-Z0-9.]', ' ',tag).lower()  
             return tags
     except:
         print("Cannot open file {}.".format(RSS_FEED))
+
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags
@@ -32,18 +40,23 @@ def get_top_tags(tags):
     return com_tags
 
 
-
 def get_similarities(tags):
     """Find set of tags pairs with similarity ratio of > SIMILAR
     Hint 1: compare each tag, use for in for, or product from itertools (already imported)
     Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
+    # Create generator of tuples of two elements from all combinations
+    # between the words in the tags list
     prod=product(tags,repeat=2)
     similar=[]
     for t in prod:
-        if t[0][0] == t[1][0]:
+        # Sort the tuple, and pass only the tuples
+        # which have words with the same first char
+        # and whcih have different words, to be checked for similarity 
+        t=sorted(t)
+        if t[0][0] == t[1][0] and t[0] != t[1]:
             s=SequenceMatcher(None,t[0],t[1])
-            if s.quick_ratio()>SIMILAR:
+            if s.quick_ratio() > SIMILAR:
                     similar.append(t)
     return similar
 
